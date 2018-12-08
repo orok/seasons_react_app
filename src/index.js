60a447a8cca8539015dee6eddaa7f4abc1542799
Orokita Ukpong <orokitaukpong@OrokMBP.local>
2018-12-08 17:49:25 -0500

    added conditional rendering

--- src/index.js
+++ src/index.js
@@ -1,32 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
 
 class App extends React.Component {
-  constructor(props){
-    super(props);
+  state = { lat: null, errorMessage: "" };
 
-    this.state = {
-      lat: null,
-      errorMessage: ''
-    }
-  }
-  render(){
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-        (position) => {
-          this.setState({late:position.coords.latitude});
-        },
-        (err) => {
-          this.setState({errorMessage:err.message});
-        }
+      position => this.setState({ lat: position.coords.latitude }),
+      err => this.setState({ errorMessage: err.message })
     );
-    return (
-      <div>Latitude:{this.state.lat}
-      <br/>
-      Error:{this.state.errorMessage}
-      </div>
-    );
+  }
+  render() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div>Latitude: {this.state.lat}</div>;
+    }
+
+    return <div>Loading!</div>;
   }
 }
 
-
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector("#root"));
