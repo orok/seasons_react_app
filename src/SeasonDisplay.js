f1f9355a447c21e24a852fa68c888ac7c817c37a
Orokita Ukpong <orokitaukpong@OrokMBP.local>
2018-12-08 19:51:57 -0500

    added logic for sun/display

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React from "react";
 
-const SeasonDisplay = () => {
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? "summer" : "winter";
+  } else {
+    return lat < 0 ? "winter" : "summer";
+  }
+};
 
-    return <div>Season Display</div>;
-}
+const SeasonDisplay = props => {
+  const season = getSeason(props.lat, new Date().getMonth());
+  const text = season === "winter" ? "Burrr its cold" : "Lets go to the beach";
+  return (
+    <h1>
+      <div>{text}</div>
+    </h1>
+  );
+};
 
 export default SeasonDisplay;
--- src/index.js
+++ src/index.js
@@ -11,12 +11,13 @@ class App extends React.Component {
       err => this.setState({ errorMessage: err.message })
     );
   }
+
   render() {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>;
     }
     if (!this.state.errorMessage && this.state.lat) {
-      return <div>Latitude: {this.state.lat}</div>;
+      return <SeasonDisplay lat={this.state.lat} />;
     }
 
     return <div>Loading!</div>;
